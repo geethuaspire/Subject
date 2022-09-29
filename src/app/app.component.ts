4fbe03189f5bb15a146327f63d809165d86245af
geethu_aspire <geethu.gopi@aspiresys.com>
2022-09-29 10:52:48 +0530

    commit message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+import { shareReplay, tap, catchError } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'Subject';
+export class AppComponent implements OnInit  {
+  name = 'Angular';
+
+  constructor(private httpClient: HttpClient) {}
+
+
+  ngOnInit() {
+      // will cause additional requests (two in total)
+      this.getData().subscribe();
+      this.getData().subscribe();
+
+      // will be shared using shareReplay (on in total)
+      const data$ = this.getData();
+      data$.subscribe();
+      data$.subscribe();
+      data$.subscribe();
+      data$.subscribe();
+      data$.subscribe();
+  }
+
+
+  private getData() {
+    return this.httpClient.get('https://www.metaweather.com/api/location/44418/')
+      .pipe(
+        catchError(() => of('request failed')),
+        tap(res => console.log('REQUEST CALLED:', res)),
+        shareReplay(1),   
+      )
+  }
 }
